d350f435f35f13dc85f46c79892d2a571b42d514
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-11-23 23:20:02 +0900

    add docker compose

--- book-api/src/test/java/com/juniordevmind/bookapi/BookApiApplicationTests.java
+++ book-api/src/test/java/com/juniordevmind/bookapi/BookApiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookApiApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
--- comment-api/src/test/java/com/juniordevmind/commentapi/CommentApiApplicationTests.java
+++ comment-api/src/test/java/com/juniordevmind/commentapi/CommentApiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommentApiApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
